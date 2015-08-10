0fd8410f0b347a5b1e482ab26a79f02cbc3af2e8
Prakhar Joshi <you@example.com>
2015-08-11 04:31:12 +0530

    setting up browser and just a simple sample test

---
+++ src/experimental/safe_html_transform/tests/test_filter.py
@@ -0,0 +1,37 @@
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from experimental.safe_html_transform.testing import \
+    EXPERIMENTAL_SAFE_HTML_TRANSFORM_FUNCTIONAL_TESTING
+# from plone.app.testing import TEST_USER_ID, setRoles
+# from plone.app.textfield.value import RichTextValue
+# from plone.app.z3cform.interfaces import IPloneFormLayer
+# from plone.dexterity.interfaces import IDexterityFTI
+from plone.testing.z2 import Browser
+# from zope.component import createObject
+# from zope.component import queryUtility
+# from zope.interface import alsoProvides
+
+# import transaction
+import unittest2 as unittest
+
+
+class DocumentFunctionalTest(unittest.TestCase):
+
+    layer = EXPERIMENTAL_SAFE_HTML_TRANSFORM_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.portal_url = self.portal.absolute_url()
+        self.browser = Browser(app)
+        self.browser.handleErrors = False
+        self.browser.addHeader(
+            'Authorization',
+            'Basic %s:%s' % (SITE_OWNER_NAME, SITE_OWNER_PASSWORD,)
+        )
+
+    def test_url_end(self):
+        self.browser.open(self.portal_url + '/@@safe_html_transform-settings')
+        self.assertTrue(
+            self.browser.url.endswith('safe_html_transform-settings'))
